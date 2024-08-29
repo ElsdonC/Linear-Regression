8a0a74d9f2a1e504fe2f647ed512f6c24c569121
chan2242 <chan2242@umn.edu>
2024-08-29 16:19:15 -0500

    converted to a class and implemented gradient descent

--- linearRegression.py
+++ linearRegression.py
@@ -1,7 +1,31 @@
-def calculate_error(initial_b, initial_m, points):
-    error = 0
-    for i in range(len(points)):
-        x, y = points[i][0], points[i][1]
-        error += (y - (initial_m * x) + initial_b)**2
-    error /= len(points)
-    return error
+class LinearRegression:
+    def __init__(self, learning_rate, points, num_iterations):
+        self.learning_rate = learning_rate
+        self.points = points
+        self.num_iterations = num_iterations
+
+    def calculate_error(self, initial_b, initial_m):
+        error = 0
+        for i in range(len(self.points)):
+            x, y = self.points[i][0], self.points[i][1]
+            error += (y - (initial_m * x) + initial_b)**2
+        error /= len(self.points)
+        return error
+
+    def gradient_descent(self, b, m):
+        b, m = b, m
+        for i in range(self.num_iterations):
+            b, m = self.gradient_descent_step(b, m)
+        return [b, m]
+
+    def gradient_descent_step(self, b, m):
+        gradient_b, gradient_m = 0, 0
+        num_points = len(self.points)
+        for i in range(num_points):
+            x, y = self.points[i][0], self.points[i][1]
+            error = (y - (m * x + b))
+            gradient_b += -(2/num_points) * error
+            gradient_m += -(2/num_points) * x * error
+        new_b = b - (self.learning_rate * gradient_b)
+        new_m = m - (self.learning_rate * gradient_m)
+        return [new_b, new_m]
